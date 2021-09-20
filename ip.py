41e1fc81bd52c0f99f21074b5fd33bbf077e7b8e
EarthlyEric6 <minecaeft.creeper.eric@gmail.com>
2021-09-20 15:54:55 +0800

    ~

---
+++ ip.py
@@ -0,0 +1,6 @@
+import csv
+
+class ip():
+
+    def get_info(ip):
+        print('hi')
---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+name = 'iptoolsbox',
+version = '0.1',
+keywords = ('simple', 'test', 'iptools'),
+description = '',
+classifiers=[
+'Development Status :: 3 – Alpha',
+'Intended Audience :: Developers',
+'Topic :: Software Development :: Libraries',
+'License :: OSI Approved :: MIT License',
+'Programming Language :: Python :: 3.9',
+],
+license = 'MIT License',
+install_requires = [''],
+author = 'EarthlyEric@ReLoad Dev',
+author_email = 'admin@reload-dev.ml',
+packages = find_packages(),
+platforms = 'any',
+)
